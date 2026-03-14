235b976e4401f4ce0f5b8e73f44717b686432613
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-14 22:22:38 +0500

    bug fixes

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "ipv4_only" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, 1) # Carves a /28 from a /24
+  vpc_id = aws_vpc.main.id
+  # Using index 4 to move away from the contested lower ranges
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, 4)
 
   tags = {
     Name = "ipv4-only-subnet"
@@ -8,8 +9,9 @@ resource "aws_subnet" "ipv4_only" {
 }
 
 resource "aws_subnet" "dual_stack" {
-  vpc_id          = aws_vpc.main.id
-  cidr_block      = cidrsubnet(aws_vpc.main.cidr_block, 4, 1) # Carves a /28 from a /24
+  vpc_id = aws_vpc.main.id
+  # Using index 5 to ensure no overlap with index 4
+  cidr_block      = cidrsubnet(aws_vpc.main.cidr_block, 4, 5)
   ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 2)
 
   assign_ipv6_address_on_creation = true
@@ -25,8 +27,10 @@ resource "aws_subnet" "ipv6_only" {
   ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 3)
 
   assign_ipv6_address_on_creation = true
+  enable_dns64                    = true
 
-  enable_dns64 = true
+  # REQUIRED for ipv6_native = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
 
   tags = {
     Name = "ipv6-only-subnet"
