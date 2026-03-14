1d7d2ef904bf14523af6e395cf24af532b429211
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-14 21:11:39 +0500

    add provider

---
+++ environments/dev/vpc/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.28"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "second"
+}
