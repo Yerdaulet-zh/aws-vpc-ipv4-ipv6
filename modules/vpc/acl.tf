354305cbaa580805b83a4edf802f4937bc84ad26
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-14 21:17:57 +0500

    add IPv6 rules

--- modules/vpc/acl.tf
+++ modules/vpc/acl.tf
@@ -5,6 +5,7 @@ resource "aws_network_acl" "main" {
   }
 }
 
+# IPv4 rules
 resource "aws_network_acl_rule" "acl_rule_ingress" {
   network_acl_id = aws_network_acl.main.id
   rule_number    = 100
@@ -12,7 +13,7 @@ resource "aws_network_acl_rule" "acl_rule_ingress" {
   protocol       = "-1"
   cidr_block     = "0.0.0.0/0"
   rule_action    = "allow"
-  from_port      = 1024
+  from_port      = 0
   to_port        = 65535
 }
 
@@ -25,6 +26,29 @@ resource "aws_network_acl_rule" "acl_rule_egress" {
   rule_action    = "allow"
   from_port      = 0
   to_port        = 65535
+}
+
+# IPv6 rules
+resource "aws_network_acl_rule" "ipv6_ingress_ephemeral" {
+  network_acl_id  = aws_network_acl.main.id
+  rule_number     = 110
+  egress          = false
+  protocol        = "-1"
+  rule_action     = "allow"
+  ipv6_cidr_block = "::/0"
+  from_port       = 0
+  to_port         = 65535
+}
+
+resource "aws_netork_acl_rule" "ipv6_egress_ephemeral" {
+  network_acl_id  = aws_network_acl.main.id
+  rule_number     = 110
+  egress          = true
+  protocol        = "-1"
+  rule_action     = "allow"
+  ipv6_cidr_block = "::/0"
+  from_port       = 0
+  to_port         = 65535
 }
 
 resource "aws_network_acl_association" "network_acl_association" {
