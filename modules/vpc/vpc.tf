dd3037754cec65550e227c6136429b9ccfc13099
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-14 21:41:59 +0500

    enable ipam pool for ipv4 address

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,10 +1,13 @@
 resource "aws_vpc" "vpc" {
-  cidr_block                           = var.vpc_cidr
+  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.ipv4.id
+  ipv4_netmask_length = 24
+
   assign_generated_ipv6_cidr_block     = true
   enable_dns_support                   = true
   enable_dns_hostnames                 = true
   enable_network_address_usage_metrics = true
+
   tags = {
-    Project = "${var.project_name}"
+    Project = var.project_name
   }
 }
